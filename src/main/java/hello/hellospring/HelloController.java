3a12b8de42956c4f58a0dd7a5b9b91e86edfb0a0
sangikHwang <hwangsangik@gamil.com>
2024-02-07 22:41:39 +0900

    Day1

---
+++ src/main/java/hello/hellospring/HelloController.java
@@ -0,0 +1,49 @@
+package hello.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { //@RequestParam -> 외부에서 파라미터 정보 받는다.
+        model.addAttribute("name", name); // key value로 구성
+        return "hello-template";
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody //객체를 반환하면 객체가 JSON으로 반환
+                  //HTML에 나오는 BodyTag X, HTTP Header & Body
+    public String helloSpring(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
